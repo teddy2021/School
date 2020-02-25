b1f86001d5cc7f4f014a35aadca1431d7e502319
Kody <teddy11@live.ca>
2020-02-25 10:52:19 -0600

    Created build function for generifying the logic of siblings on down. Implemented siblings and first cousins with build.

--- 340A3.1/src/Q1.scala
+++ 340A3.1/src/Q1.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object Q1 {
 
   val royalParent = Map("George" -> ("m", "William", "Catherine"), "Charlotte" -> ("f", "William", "Catherine"), "Louis" -> ("m", "William", "Catherine"), "Archie" -> ("m", "Harry", "Meghan"), "Savannah" -> ("f", "Autumn", "Peter"), "Isla" -> ("f", "Autumn", "Peter"), "Mia" -> ("f", "Zara", "Mike"), "Lena" -> ("f", "Zara", "Mike"), "Beatrice" -> ("f", "Andrew", "Sarah"), "Eugenie" -> ("f", "Andrew", "Sarah"), "Louise" -> ("f", "Edward", "Sophie"), "James" -> ("m", "Edward", "Sophie"), "Peter" -> ("m", "Mark", "Anne"), "Zara" -> ("f", "Mark", "Anne"), "William" -> ("m", "Diana", "Charles"), "Harry" -> ("m", "Diana", "Charles"), "Charles" -> ("m", "Elizabeth", "Philip"), "Anne" -> ("f", "Elizabeth", "Philip"), "Andrew" -> ("m", "Elizabeth", "Philip"), "Edward" -> ("m", "Elizabeth", "Philip"), "Elizabeth" -> ("f", "", ""), "Philip" -> ("m", "", ""), "Diana" -> ("f", "", ""), "Mark" -> ("m", "", ""), "Sophie" -> ("f", "", ""), "Sarah" -> ("f", "", ""), "Mike" -> ("m", "", ""), "Autumn" -> ("f", "", ""), "Meghan" -> ("f", "", ""), "Catherine" -> ("f", "", ""), "Timothy" -> ("m", "", ""), "Jack" -> ("m", "", ""), "Camilla" -> ("f", "", ""))
@@ -6,7 +8,7 @@ object Q1 {
   def parents(p: String): Option[(String, String)] = {
     val res = royalParent.get(p)
     val rents = res.getOrElse((Nil,Nil,Nil))
-    if(res == None){
+    if(res.isEmpty){
       None
     }
     else{
@@ -22,47 +24,43 @@ object Q1 {
 
   def grandparents(p: String): Option[List[String]] = royalParent.get(p) match{
     case None => None
-    case Some((_, a, b: String)) => {
+    case Some((_, a, b: String)) =>
       val gmat: List[String] = parents(a).map(t => List(t._1, t._2)).getOrElse(List("Untracked", "Untracked"))
       val gpat: List[String] = parents(b).map(t=>List(t._1, t._2)).getOrElse(List("Untracked", "Untracked"))
-      println(gmat, "aaa", gpat)
-      Some(List(gmat(0), gmat(1), gpat(0), gpat(1)))
-     }
+      Some(List(gmat.head, gmat(1), gpat.head, gpat(1)))
+
+  }
+
+
+  @tailrec
+  def build[A, B](p: String, group: List[String], default_return: List[String], default: A)(h: String => Option[A])(f: (A, A) => Boolean): List[String] = group match{
+    case Nil => default_return
+    case a::b =>
+      val first = h(p).getOrElse(default)
+      val second = h(a).getOrElse(default)
+      if(f(first, second)){
+        build(p, b, a::default_return, default)(h)(f)
+      }
+      else{build(p, b, default_return, default)(h)(f)}
+
   }
 
   def siblings(p: String): Option[List[String]] = {
-    val parent = parents(p).getOrElse(("Untracked", "Untracked"))
-    val family = royalParent.keySet.toList
-    def go(ppl: List[String], default: List[String]): Option[List[String]] ={
-
-      if(ppl != Nil) {
-        val sz = ppl.size
-        val child = ppl(0)
-        val ancestor = parents(child).getOrElse(("Untracked", "Untracked"))
-        if (child != p && parent == ancestor) {
-          go(ppl.slice(1, sz), child::default)
-        }
-        else{
-          go(ppl.slice(1, sz), default)
-        }
-      }
-      else{
-        if(default == List()){None}
-        else{Some(default)}
-      }
-
-    }
-    go(family, List())
+    val family = royalParent.keySet.toList.filter(s => s != p)
+    Some(build(p, family, List(), ("Untracked", "Untracked"))(parents)((x,y) => x==y && x != ("Untracked", "Untracked")))
   }
 
-
-
   def firstCousins(p: String): Option[List[String]] = {
-    Some(List("Dog"))
+    val family = royalParent.keySet.toList.filter(s => s != p && !siblings(p).contains(s))
+    val z: List[String] = Nil
+    Some(build(p, family, z, z)(grandparents)((x, y) => x.filter(!y.contains(_)) == Nil).filter(!siblings(p).getOrElse(Nil).contains(_)))
   }
 
   def uncles(p: String): Option[List[String]] = {
+    val family = royalParent.keySet.toList.filter(s => s != p)
+    val z: List[String] = Nil
     Some(List("Dog"))
+
   }
 
   def main(args: Array[String]): Unit = {
