99e22cc626bf9656cf5637cf5afb517f6f2b8468
Kody <teddy11@live.ca>
2020-03-01 19:37:49 -0600

    End of night commit.

--- 340A3.1/src/Q2.scala
+++ 340A3.1/src/Q2.scala
@@ -15,61 +15,53 @@ object Q2 {
       case Errors(a) => Errors(a)
     }
 
-    def flatMap[AA >: A, X](f: B => Partial[AA, X]): Partial[AA, X] = this match {
-      case Success(a) => f(a)
-      case Errors(b) => Errors(b)
+    def flatMap[AA >: A, BB >: B](f: B => Partial[AA, BB]): Partial[AA, BB] = this match {
+      case Success(b) => f(b)
+      case Errors(a) => Errors(a)
     }
 
-    def getOrElse[C >: B](default: => C): C = this match {
-      case Success(a) => a
-      case Errors(b) => default
+    def getOrElse[BB >: B](itm: BB):BB = this match{
+      case Success(b) => b
+      case Errors(_) => itm
     }
 
-    def orElse[AA >: A, BB >: B](f: => Partial[AA, BB]): Partial[AA, BB] = this match {
-      case Success(a) => Success(a)
-      case Errors(b) => f
+    def orElse[AA >: A, BB >:B](itm: Partial[AA, BB])(f: => Partial[AA, BB]): Partial[AA, BB] = itm match {
+      case Success(b) => Success(b)
+      case Errors(_) => f
     }
 
-    def map2[AA >: A, C, D](b: Partial[AA, C])(f: (B, C) => D): Partial[AA, D] = {
+    def map2[AA >: A, C, D](c1: Partial[AA, C])(f: (B, C) => D): Partial[AA, Int] =
       for {
-        a <- this;
-        c <- b
-      } yield f(a, c)
+        b <- this
+        c <- c1// claims c1 is of type Q2.Partial[AA, D]
+      } yield f(b, c)
+
+    def traverse[A, B, C](lst: List[C])(f: C => Partial[A, B]): Partial[A, List[B]] = lst match {
+      case Nil => Errors(Nil)
+      case h::t => (f(h) map2 traverse(t)(f))(_ :: _) // Claims is of type Q2.Partial[A, Int]
     }
 
-    def traverse[A, B, C](itm: List[B])(f: B => Partial[A, C]): Partial[A, List[C]] =
-      itm.foldRight[Partial[A, List[C]]](Success(Nil))((a, b) => f(a).map2(b)(_ :: _))
-
-    /*
-    def Try[B](f: =>B): Partial[A, B] = {
-      try Success(f)
-      catch {
-        case e: Exception => Errors()
-      }
-    }
-  }
-*/
   }
 
   case class Errors[+A](get: Seq[A]) extends Partial[A, Nothing]
 
   case class Success[+B](get: B) extends Partial[Nothing, B]
 
-    def genList(x: Int, default: List[Int]): List[Int] = {
-      if(x > 0) genList(x -1, (1+ Random.nextInt(100) - Random.nextInt(110))::default)
-      else (default)
+  def genList[A](x: Int, default: List[Int]): List[Int] = {
+    if(x > 0) genList(x -1, (1+ Random.nextInt(100) - Random.nextInt(110))::default)
+    else default
+  }
+
+  def Loop(n: Int): Unit = {
+    if(n > 0) {
+      println("Iteration: " + (6-n))
+      val consider = Success(genList(5 + Random.nextInt(20), Nil))
+      println("List: " + consider)
+      Loop(n - 1)
     }
+  }
 
-    def Loop(n: Int): Unit = {
-      if(n > 0) {
-        println("Iteration: " + n)
-        val consider = genList(5 + Random.nextInt(20), Nil)
-
-        Loop(n - 1)
-      }
-    }
-
-    def main(args: Array[String]): Unit = {
-      Loop(5)
+  def main(args: Array[String]): Unit = {
+    Loop(5)
   }
 }
